dded6ea28bfe28f5d35b3705f762c3ef9eb003a1
1Njeru <122145897+1Njeru@users.noreply.github.com>
2023-06-29 23:24:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import AnimRoutes from './components/AnimRoutes';
 import React from 'react';
 
 import {BrowserRouter as Router} from 'react-router-dom'
-import { motion } from 'framer-motion'
-import About from './pages/Abot';
-import Portfolio from './pages/Portfolio';
+
 
 
 const App = () => {
